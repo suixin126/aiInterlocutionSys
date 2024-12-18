dbe5375a021b7a45f6df068cd5e5022d887e955d
suixin126 <3477925517@qq.com>
2024-12-18 11:43:13 +0800

    路由更改

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,39 @@
-import { createWebHashHistory, createRouter } from 'vue-router'
-
-
+import { createWebHashHistory, createRouter } from "vue-router";
 
 const routes = [
-    {
-        path:'/login',
-        name:'login',
-        component: () => import('@/views/login/Login.vue')
-    }
-]
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("@/views/login/Login.vue"),
+  },
+  {
+    path: "/",
+    name: "home",
+    redirect:'/study_and_practice',
+    component: () => import("@/views/home/Home.vue"),
+    children: [
+      {
+        path:'study_and_practice',
+        name:'study_and_practice',
+        component:()=>import('@/views/study_and_practice/index.vue')
+      },
+      {
+        path: "/study",
+        name: "study",
+        component: () => import("@/views/study/Study.vue"),
+      },
+      {
+        path:'/practice',
+        name:'practice',
+        component:()=>import('@/views/study/Practice.vue')
+      }
+    ],
+  },
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
-})
+});
 
-export default router
+export default router;
