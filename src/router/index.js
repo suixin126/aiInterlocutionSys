8a3855d2d46f06fc3871062b720b50bef7f7fdc6
LiJie <2360068539@qq.com>
2024-12-18 11:38:47 +0800

    resetpassword

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,16 @@ import { createWebHashHistory, createRouter } from 'vue-router'
 
 
 const routes = [
-    {
-        path:'/login',
-        name:'login',
-        component: () => import('@/views/login/Login.vue')
-    }
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/login/Login.vue')
+  },
+  {
+    path: '/resetPassword',
+    name: 'resetPassword',
+    component: () => import('@/views/resetpassword/ResetPassword.vue')
+  }
 ]
 
 const router = createRouter({
